78c8c3a4facccf77386a0e612b735dd478ad859d
MicheleStaffiere <michelestaffiere1@gmail.com>
2023-08-09 03:38:07 -0400

    randomize button works, needs fine tuning on items array to prevent dupes, some champion champons grant 403 error code.

--- src/Randomizer.jsx
+++ src/Randomizer.jsx
@@ -36,8 +36,9 @@ const DataParser = () =>{
         const champion = champions[key];
         const data ={
           'name': `${champion.name}`,
-          'title': `${champion.title}`,
+          'title': `${champion.title}`,S
           'key': `${champion.key}`,
+          'img' : `${champion.image.full}`
         }
         parsedChampions.push(data);
       });
@@ -67,90 +68,127 @@ const DataParser = () =>{
     setParsedData(false);
     }
   },[loadingAPI]);
- return [champFinal,itemsFinal]
+ return [champFinal,itemsFinal,versionNumber]
 };
 
 
 // actual randomizer function - takes in two arrays and returns a random champion and 6 random items.
-const rtd = (champArray,itemArray) =>{
+const rtd = (champArray, itemArray, setRanChamp , setRanItems) => {
   const items = [];
   const champion = [];
-  let mythic = false;
-  let boots = false;
-
- //WORKS RANDOM CHAMPION//
-  let randomIndex = Math.floor(Math.random()*champArray.length);
+  // Random Champion
+  let randomIndex = Math.floor(Math.random() * champArray.length);
   champion.push(champArray[randomIndex]);
 
-
- //RANDOM ITEMS
-
- while (items.length < 6) {
-   let randomIndex = Math.floor(Math.random() * itemArray.length);
-   let randomItem = itemArray[randomIndex];
   
-   if(
-    !boots && 
-    Object.prototype.hasOwnProperty.call(randomItem.tags,'Boots') &&
-    !Object.prototype.hasOwnProperty.call(randomItem, 'Into')
-    ){
-    items.push(randomItem);
-    boots = true;
-   } else if(
-    boots === true &&
-    !mythic &&
-    !Object.prototype.hasOwnProperty.call(randomItem.tags, 'Boots') &&
-    randomItem.description.indexOf('Mythic') !== -1 ||
-    randomItem.description.indexOf('Mythic Passive') !== -1 
-   ){
-    items.push(randomItem);
-   } else if(
-    !Object.prototype.hasOwnProperty.call(randomItem, 'Into')
-   ){
-    items.push(randomItem);
-   }
- }
- console.log(items);
+  //Random Boots
+  const bootsItems = itemArray.filter(item => item.tags.includes('Boots') && !Object.prototype.hasOwnProperty.call(item, 'into'));
+  let randomBootIndex =  Math.floor(Math.random() * bootsItems.length);
+  let randomBoot = bootsItems[randomBootIndex];
+  items.push(randomBoot);
+
+  // Random Mythics
+  const mythicItems = itemArray.filter(item => 
+    item.description.includes('Mythic') ||
+    item.description.includes('mythic') ||
+    item.description.includes('Mythic passive') ||
+    item.description.includes('Mythic Passive'));
+  let randomMythicIndex = Math.floor(Math.random() * mythicItems.length);
+  let randomMythic = mythicItems[randomMythicIndex];
+  items.push(randomMythic);
+  // Random legendary Items
+  while (items.length < 6) {
+    randomIndex = Math.floor(Math.random() * itemArray.length);
+    const randomItem = itemArray[randomIndex];
+    const hasInto = Object.prototype.hasOwnProperty.call(randomItem, 'into');
+    const hasBootsTag = randomItem.tags.includes('Boots');
+    const hasMythicDecsirption =  
+    randomItem.description.includes('Mythic') ||
+    randomItem.description.includes('mythic') ||
+    randomItem.description.includes('Mythic passive') ||
+    randomItem.description.includes('Mythic Passive');
+
+    if(!hasInto && !hasBootsTag && !hasMythicDecsirption){
+      items.push(randomItem);
+    }
+  }
+  setRanChamp(champion);
+  setRanItems(items);
+  // console.log(champion)
+  // console.log(items);
 };
 
 
-const Randomizer = () =>{
-  const [c,i] = DataParser();
-  const [champs,setChamps] = useState([]);
+const Randomizer = () => {
+  const [c, i, v] = DataParser();
+  const [champs, setChamps] = useState([]);
   const [items, setItems] = useState([]);
+  const [version, setVersion] = useState([]);
+  const [randomChamp, setRanChamp] = useState([]);
+  const [randomItems, setRanItems] = useState([]);
+  
 
+  useEffect(() => {
+    setChamps(c);
+    setItems(i);
+    setVersion(v)
+  }, [c, i, v]);
 
-  useEffect(()=>{
-  setChamps(c);
-  setItems(i)
-  },[c,i]);
+  const handleRollClick = () => {
+    rtd(champs, items, setRanChamp, setRanItems);
+  };
 
-return (
-  <>
- {champs.length === 0 ? <div><p>Loading the rift!</p><img src="/src/assets/loading-app.gif" alt="Loading Walk" /></div>:
- 
- <section className="randomizerContainer">
-      <button onClick={()=>{rtd(champs,items)}}>Roll the dice!</button>
-
-
-
-
+  const handleRandomDisplay = (randomChamp,randomItems) =>{
+  let champ = randomChamp;
+  let items = randomItems;
+  let imgEndPoint = "https://ddragon.leagueoflegends.com/cdn/12.4.1/img/champion/" ;
+    return(
       <div className="randomizedData">
-        <div className="championImg">
-          <img src="" alt="" />
+            <div className="championImg">
+
+            </div>
+            <div className="championInfo">
+              <h2>{champ[0].name}</h2>
+              <p>{champ[0].title}</p>
+              <img src={imgEndPoint + champ[0].img} alt={`${champ[0].name} has been rolled`}/>
+            </div>
+            <div className="items">
+              <ul>
+                {
+                  items.map((item)=>{
+                    let endpoint = `https://ddragon.leagueoflegends.com/cdn/${version}/img/item/${item.image.full}`;
+                    return(
+                        <li>
+                          <img src={endpoint} alt={item.name} />
+                        </li>
+                    )
+                  })
+                }
+              </ul>
+              </div>
+          </div>
+    )
+  };
+  return (
+    <>
+      {champs.length === 0 ? (
+        <div>
+          <h2>Loading the rift!</h2>
+          <img src="/src/assets/loading-app.gif" alt="Loading Walk" />
         </div>
-        <img src="" alt="" />
-        <div className="championInfo">
-          <h2>{}</h2>
-          <p>{}</p>
-        </div>
-        <div className="items">
-          {/* map through the items array here to display to page. */}
-        </div>
-      </div>
-    </section>
-    }
-  </>
-)
+      ) : (
+        <section className="randomizerContainer">
+          <button onClick={handleRollClick}>Roll the dice!</button>
+          { randomChamp.length ===  0 ? (
+            <h2>Getting Spicy</h2>
+          ) : (
+            <> 
+            {handleRandomDisplay(randomChamp,randomItems)}
+            </>
+          )}
+        </section>
+      )}
+    </>
+  );
 };
 export default Randomizer
